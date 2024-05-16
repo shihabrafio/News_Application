cfb2dc0486e340b83f702193ff991db7ed0ad8ec
shihabrafio <aimshihabuddinkhan@gmail.com>
2024-05-16 18:21:20 +0600

    Update npm dependencies and add NewsListScreen and NewsDetailScreen components

---
+++ App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Ionicons } from '@expo/vector-icons'; // Import Ionicons from @expo/vector-icons
+import NewsListScreen from './src/screens/NewsListScreen';
+import NewsDetailScreen from './src/screens/NewsDetailScreen';
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="NewsListScreen"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#fff', // Customize header background color
+          },
+          headerTintColor: '#000', // Customize header text color
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      >
+        <Stack.Screen
+          name="News_Application"
+          component={NewsListScreen}
+          options={{
+            title: 'News Application',
+            headerRight: () => (
+              <Ionicons
+                name="newspaper-outline"
+                size={24}
+                color="#000"
+                style={{ marginRight: 20 }}
+              />
+            ),
+          }}
+        />
+        <Stack.Screen
+          name="NewsDetailScreen"
+          component={NewsDetailScreen}
+          options={{
+            title: 'News Detail',
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
